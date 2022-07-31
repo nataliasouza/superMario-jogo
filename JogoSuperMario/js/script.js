0f422c9f2a24331136a3a9d850244a50dcb22abf
Natalia Souza <nataliasouza.ti@hotmail.com>
2022-07-31 15:40:06 -0300

    Adicionei Score e músicas

--- JogoSuperMario/js/script.js
+++ JogoSuperMario/js/script.js
@@ -1,19 +1,31 @@
 const mario = document.querySelector(".super-mario");
 const pipe = document.querySelector(".pipe-game");
+const audioJump = new Audio("./audio/mario_pulando.mp3");
+const audioGameOver = new Audio("./audio/gameOver.mp3");
+const audioStart = new Audio("./audio/musica-fundo.mp3");
+const scoreGame = document.querySelector(".score");
 
-const jump = () => {
-    mario.classList.add("jump-mario");
+audioStart.play();
 
-    setTimeout(() =>{
-        mario.classList.remove("jump-mario");
-    }, 500);
-};
+const loopGame = setInterval(() => {        
 
-const loopGame = setInterval(() => {
+    const jump = () => {
+        mario.classList.add("jump-mario");
+    
+        setTimeout(() => {
+            mario.classList.remove("jump-mario");
+        }, 500);
+    
+        audioJump.play();
+    };
+    
+    document.addEventListener("keydown", jump);
+    document.addEventListener("click", jump);    
+
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window
         .getComputedStyle(mario)
-        .bottom.replace("px", "");
+        .bottom.replace("px", "");        
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = "none";
@@ -24,11 +36,43 @@ const loopGame = setInterval(() => {
 
         mario.src = "./img/mario-game-over.png";
         mario.style.width = "75px";
-        mario.style.marginLeft = "45px";
+        mario.style.marginLeft = "45px"; 
 
+        audioJump.pause();
+        audioStart.pause();
+        audioGameOver.play();
+        
         clearInterval(loopGame)     
     }
 
 }, 10);
+
+    let score = 0;
+    let cross = true;
+
+    setInterval(() => {    
+        
+        const pipePosition = pipe.offsetLeft;
+        const marioPosition = +window
+            .getComputedStyle(mario)
+            .bottom.replace("px", "");
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 115){
+        if (score != 0)
+            scoreGame.innerHTML = "Score " + score;
+    } else if (marioPosition > 115 && cross) {
+        score += 10;
+        updateScore(score);
+        cross = false;
+        setTimeout(() => {
+            cross = true;
+        }, 1000);
+    }
+    }, 10);
+
+    function updateScore(score) {
+    scoreGame.innerHTML = "Score " + score;
+    }
+
+
     
-document.addEventListener("keydown", jump);
